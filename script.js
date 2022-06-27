04dc6bfb5b6b8413847c1de15e730f33470a2fc9
Daringu <97928219+Daringu@users.noreply.github.com>
2022-06-27 15:13:14 +0300

    cart
    the best cart

---
+++ script.js
@@ -0,0 +1,89 @@
+class Product {
+
+
+    constructor(namee, price) {
+        this.name = namee
+        this.price = price
+    }
+}
+const cart = {
+    items: [],
+    getItems() {
+        return this.items
+    },
+    getTotalPrice() {
+        let total = 0;
+        if (this.items.length === 0) {
+            return 'you have not chosen anything yet'
+        }
+        for (const item of this.items) {
+            let price = item.price
+            if (item.quantity >= 10) {
+                price = price * 0.5;
+            }
+            total += price * item.quantity;
+        }
+        return total
+    },
+    addItem(object, quantity) {
+        if (quantity === undefined) {
+            quantity = 1
+        }
+        const newObject = {
+            ...object,
+            quantity: quantity
+        }
+        for (const item of this.items) {
+            if (item.name === newObject.name) {
+                return item.quantity += quantity
+
+            }
+        }
+        this.items.push(newObject)
+    },
+    removeItem(name) {
+        this.items.splice(this.items.indexOf(name), 1)
+    },
+    updateCart() {
+        this.items = [];
+    },
+    increase(name, quantity) {
+        if (quantity === undefined) {
+            quantity = 1
+        }
+        for (const item of this.items) {
+            if (item.name === name) {
+                if (item.quantity >= 1) {
+                    item.quantity += quantity
+                }
+            }
+        }
+    },
+    decrease(name, quantity) {
+        if (quantity === undefined) {
+            quantity = 1
+        }
+        for (const item of this.items) {
+            if (item.name === name) {
+                if (item.quantity > quantity) {
+                    return item.quantity -= quantity
+                } else {
+                    this.removeItem(name)
+                }
+            }
+        }
+    }
+
+}
+
+const lemone = new Product('lemone', 100)
+const strawberry = new Product('strawberry', 200)
+cart.addItem(strawberry)
+cart.addItem(strawberry, 4)
+cart.decrease(strawberry.name, 4)
+cart.increase(strawberry.name, 98)
+cart.addItem(lemone, 99)
+cart.decrease(lemone.name, 99)
+cart.removeItem(strawberry.name)
+console.log(cart.getTotalPrice());
+console.log(cart.getItems());
